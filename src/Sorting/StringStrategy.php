9ed4ecb3ae62ae8514bb36b43745d5c5c9475dcd
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-03-15 19:22:42 +0100

    Use @template for better auto-completion and static analysis

--- src/Sorting/StringStrategy.php
+++ src/Sorting/StringStrategy.php
@@ -3,17 +3,15 @@
 namespace App\Sorting;
 
 
+/**
+ * @implements SortingStrategyInterface<string>
+ */
 class StringStrategy implements SortingStrategyInterface {
 
     private function transliterate(string $string): string {
         return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
     }
 
-    /**
-     * @param T $objectA
-     * @param T $objectB
-     * @return int
-     */
     public function compare($objectA, $objectB): int {
         return strnatcasecmp($this->transliterate((string)$objectA), $this->transliterate((string)$objectB));
     }
