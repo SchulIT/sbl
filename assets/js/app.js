be37f6d6e32cb58d71196e2c68139211b231783f
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-03-15 19:21:51 +0100

    Fix asset loading + adds table-collapse

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,10 @@ require('../css/app.scss');
 
 import { Modal, Tooltip, Popover } from "bootstrap";
 
-require('../../vendor/schulit/common-bundle/Resources/assets/js/polyfill');
-require('../../vendor/schulit/common-bundle/Resources/assets/js/menu');
-require('../../vendor/schulit/common-bundle/Resources/assets/js/dropdown-polyfill');
+require('../../vendor/schulit/common-bundle/assets/js/polyfill');
+require('../../vendor/schulit/common-bundle/assets/js/menu');
+require('../../vendor/schulit/common-bundle/assets/js/dropdown-polyfill');
+require('../../vendor/schulit/common-bundle/assets/js/table-collapse');
 
 document.addEventListener('DOMContentLoaded', function() {
     let initializeChoice = function(el) {
